33f1a05493599146d9ee803304ab1e6098fa0ae8
mateus.castro <castro@oceanainvestimentos.com.br>
2022-02-24 17:02:51 -0300

    visibilidade dos métodos e enums

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -2,9 +2,7 @@ import { Negociacoes } from "../models/negociacoes.js"
 import { View } from "./view.js"
 
 export class NegociacoesView extends View<Negociacoes>{
-    
-
-    template(model: Negociacoes): string {
+    protected template(model: Negociacoes): string {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -19,7 +17,7 @@ export class NegociacoesView extends View<Negociacoes>{
                         model.lista().map((negociacao) => {
                             return `
                                 <tr>
-                                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                    <td>${this.formatar(negociacao.data)}</td>
                                     <td>${negociacao.quantidade}</td>
                                     <td>${negociacao.valor}</td>
                                 </tr>
@@ -29,5 +27,10 @@ export class NegociacoesView extends View<Negociacoes>{
                 </tbody>
             </table>
         `
+    }
+
+    private formatar(data: Date){
+        return new Intl.DateTimeFormat()
+            .format(data)
     }
 }
