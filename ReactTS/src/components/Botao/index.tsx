1cde23c7f7e84f80a824a595a6690d7a06690307
mateus.castro <castro@oceanainvestimentos.com.br>
2022-03-02 17:42:42 -0300

    Criando os primeiros class (antigo) e function (novo) components.

---
+++ ReactTS/src/components/Botao/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Botao extends Component {
+    render() {
+        return (
+            <button>
+                Botão
+            </button>
+        )
+    }
+}
+
+export default Botao
